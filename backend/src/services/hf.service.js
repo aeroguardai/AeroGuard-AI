0abefff3aaf0c93cc05c596724fa46aa5fa8f75e
aeroguardai <aiaeroguard@gmail.com>
2025-11-25 16:35:54 +0530

    Update hf.service.js

--- backend/src/services/hf.service.js
+++ backend/src/services/hf.service.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
+export const callHFModel = async (prompt) => {
+  const response = await axios.post(
+    'https://api-inference.huggingface.co/models/meta-llama/Llama-3-8b',
+    { inputs: prompt },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.HF_API_KEY}`,
+      },
+    }
+  );
+
+  return response.data[0].generated_text;
+};
 
