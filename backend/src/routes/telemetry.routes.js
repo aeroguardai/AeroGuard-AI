49e32ee0cfeebd0baccf6ca97bcb620a15ba6a9a
aeroguardai <aiaeroguard@gmail.com>
2025-11-27 20:40:06 +0530

    Update telemetry.routes.js

--- backend/src/routes/telemetry.routes.js
+++ backend/src/routes/telemetry.routes.js
@@ -1,14 +1,19 @@
 import express from 'express';
-import {
-  postTelemetry,
-  getLatestTelemetry,
-  getTelemetryHistory
+import { 
+    postTelemetry, 
+    getLatestTelemetry,
+    getTelemetryHistory 
 } from '../controllers/telemetry.controller.js';
 
 const router = express.Router();
 
+// POST API (Store telemetry)
 router.post('/', postTelemetry);
+
+// GET Latest telemetry
 router.get('/latest', getLatestTelemetry);
+
+// GET history
 router.get('/history', getTelemetryHistory);
 
 export default router;
