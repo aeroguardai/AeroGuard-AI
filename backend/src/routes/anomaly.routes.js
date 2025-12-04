ca44864845d28a13cd2e3e7625db172c9bbfe409
aeroguardai <aiaeroguard@gmail.com>
2025-12-04 19:43:47 +0530

    Update anomaly.routes.js

--- backend/src/routes/anomaly.routes.js
+++ backend/src/routes/anomaly.routes.js
@@ -1,9 +1,8 @@
-// anomaly.routes.js
-import express from 'express';
-import { detectAnomaly } from '../controllers/anomaly.controller.js';
+import express from "express";
+import { detectAnomaly } from "../controllers/anomaly.controller.js";
 
 const router = express.Router();
 
-router.post('/predict', detectAnomaly);
+router.post("/predict", detectAnomaly);
 
 export default router;
